2f2fcb27e7df7e574991f5cd32c5717370f667d3
RobertoVillani <robertovillani58@gmail.com>
2022-02-28 11:53:29 +0100

    add page not found

---
+++ resources/js/guest/pages/PageNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Pagina non trovata</h1>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
+
+<style>
+</style>
--- resources/js/guest/router.js
+++ resources/js/guest/router.js
@@ -6,6 +6,8 @@ Vue.use(VueRouter);
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SinglePost from "./pages/SinglePost";
+import PageNotFound from "./pages/PageNotFound";
+
 
 
 const router = new VueRouter({
@@ -25,6 +27,11 @@ const router = new VueRouter({
             path: "/posts/:slug",
             name: "single-post",
             component: SinglePost
+        },
+        {
+            path: "*",
+            name: "page-404",
+            component: PageNotFound
         },
     ]
 });
